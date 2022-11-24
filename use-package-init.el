b47b9796fa35686c43a86343ac60f04fb3e6e250
Krzysztof Kielak <krzysztof.kielak@gmail.com>
2022-11-24 08:49:39 +0100

    ocaml and haskell changes in init.el

--- use-package-init.el
+++ use-package-init.el
@@ -40,6 +40,9 @@
 
     ;; Support for SQLFormatter
     sqlformat
+
+    ;; Haskell
+    haskell-mode
     ))
 
 ;; On OS X, an Emacs instance started from the graphical user
@@ -142,6 +145,11 @@
          ("M-{" . paredit-wrap-curly))
   :diminish nil)
 
+(use-package haskell-mode
+  :ensure t
+  :init
+  (add-hook 'haskell-mode-hook 'interactive-haskell-mode))
+
 (use-package mic-paren)
 
 (use-package sly
@@ -188,3 +196,25 @@
 ;; Make company aware of merlin
 (with-eval-after-load 'company
  (add-to-list 'company-backends 'merlin-company-backend))
+
+;; make and relaod utop
+;; (TODO: issue with TAB completion after reload)
+(defun make-and-reload ()
+  (interactive)
+  (let ((cbuff (current-buffer))
+	(makefile-path (locate-dominating-file "." "Makefile")))
+    (if makefile-path
+	(progn
+	  (compile (concat "cd " makefile-path " && make -k"))
+	  (utop-kill)
+
+	  ;; restart utop
+	  (let ((buf (get-buffer-create utop-buffer-name))
+		(cmd utop-command))
+	    (pop-to-buffer buf)
+	    (setq utop-command cmd)
+	    (with-current-buffer buf (utop-mode))))
+      (message "No Makefile found"))))
+
+(global-set-key (kbd "C-c C-v") 'make-and-reload)
+
